a27c78a14e9b54acec6f7c660067cfa5bcfd1c96
ahmednbail <116032681+ahmednbail@users.noreply.github.com>
2026-02-03 22:21:09 +0200

    Remove unused script file and update init_database.sql to improve formatting and organization of database creation and schema definitions.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -6,19 +6,22 @@ BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE DataWarehouse;
 END;
-
 GO
+
 CREATE DATABASE DataWarehouse;
-
-USE DataWarehouse;
-
-
 GO
+USE DataWarehouse;
+GO
+
 CREATE SCHEMA bronze;
 GO
+
 CREATE SCHEMA silver;
 GO
+
 CREATE SCHEMA gold;
 GO
+
+
 
 
